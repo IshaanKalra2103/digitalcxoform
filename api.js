9d861d988f3d910a27904bbc42f7db489950e026
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2023-06-21 22:28:03 +0530

    First
    This is the first commit

---
+++ api.js
@@ -0,0 +1,13 @@
+function SubForm() {
+    $.ajax({
+      url: "https://api.apispreadsheets.com/data/bO17CLXBbp6QdAG0/",
+      type: "post",
+      data: $("#myForm").serializeArray(),
+      success: function () {
+        alert("Form Data Submitted :)");
+      },
+      error: function () {
+        alert("There was an error :(");
+      },
+    });
+  }
