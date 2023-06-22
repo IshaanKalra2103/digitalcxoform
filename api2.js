268686085481ba5fed7f6cd48d142d3934d98f4b
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2023-06-22 21:29:27 +0530

    second
    d

---
+++ api2.js
@@ -0,0 +1,29 @@
+function SubForm() {
+    // Collect form data
+    var formData = {
+      "Email Address": $("#email").val(),
+      "Your Name": $("#name").val(),
+      "Mobile number": $("#phone").val(),
+      "Your LinkedIn link": $("#linkedin").val(),
+      "Your current company": $("#company").val(),
+      "Your designation or role": $("#role").val(),
+      "Functional area": $("#functional-area").val(),
+      "Top 3 skills": $("#skills").val(),
+      "Any other areas of interest that you want from this forum": $("#other").val()
+    };
+  
+    // Send formData as a single set to the spreadsheet API
+    $.ajax({
+      url: "https://api.apispreadsheets.com/data/xVbeZbneJLZ9oGzA/",
+      type: "post",
+      data: JSON.stringify(formData), // Convert to JSON string
+      contentType: "application/json", // Set content type to JSON
+      success: function () {
+        alert("Form Data Submitted :)");
+      },
+      error: function () {
+        alert("There was an error :(");
+      },
+    });
+  }
+  
--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-function SubForm (){
-    $.ajax({
-        url:'https://api.apispreadsheets.com/data/bO17CLXBbp6QdAG0/',
-        type:'post',
-        data:$("#myForm").serializeArray(),
-        success: function(){
-          alert("Form Data Submitted :)")
-        },
-        error: function(){
-          alert("There was an error :(")
-        }
-    });
-}
+// $(document).ready(function() {
+
+//   let counter;
+//   // Hide all sections except the first one
+//   $('.form-group').not('.active').hide();
+
+//   $('.next-btn').click(function(event) {
+//       // Prevent the default action
+//       event.preventDefault();
+
+//       var currentSection = $(this).closest('.form-group');
+//       currentSection.hide();
+//       currentSection.next('.form-group').show();
+//       counter++;
+//   });
+
+//   if (counter === 8){
+//     $('.btn').removeClass('hide');
+//     $('.btn').addClass('reveal');
+//   }
+// });
+
+$(document).ready(function() {
+
+  let counter = 1;
+  // Hide all sections except the first one
+  $('.form-group').not('.active').hide();
+
+  $('.next-btn').click(function(event) {
+      // Prevent the default action
+      event.preventDefault();
+
+      var currentSection = $(this).closest('.form-group');
+      currentSection.fadeOut(400, function() {
+          var nextSection = currentSection.next('.form-group');
+          nextSection.fadeIn(400);
+
+          counter++;
+      });
+
+      console.log(counter);
+
+      if (counter === 8){
+            $('.btn').removeClass('hide');
+            $('.btn').addClass('reveal');
+          }
+  });
+
+  
+});
