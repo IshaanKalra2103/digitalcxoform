f643f49dde044274e5b990855aff73e8fde14c69
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2023-06-23 17:22:18 +0530

    major
    2

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
-// $(document).ready(function() {
+$(document).ready(function () {
+  var formGroups = $(".form-group");
+  var currentIndex = 0;
 
-//   let counter;
-//   // Hide all sections except the first one
-//   $('.form-group').not('.active').hide();
+  function showCurrentGroup() {
+    formGroups.fadeOut(500);
+    formGroups.eq(currentIndex).delay(500).fadeIn(500);
+  }
 
-//   $('.next-btn').click(function(event) {
-//       // Prevent the default action
-//       event.preventDefault();
+  $("#arrow-up").click(function () {
+    if (currentIndex > 0) {
+      currentIndex--;
+      console.log(currentIndex);
+    }
 
-//       var currentSection = $(this).closest('.form-group');
-//       currentSection.hide();
-//       currentSection.next('.form-group').show();
-//       counter++;
-//   });
+    if (currentIndex != 8) {
+      $(".btn").addClass("hide");
+      $(".next-btn").addClass("reveal");
+    }
 
-//   if (counter === 8){
-//     $('.btn').removeClass('hide');
-//     $('.btn').addClass('reveal');
-//   }
-// });
-
-$(document).ready(function() {
-
-  let counter = 1;
-  // Hide all sections except the first one
-  $('.form-group').not('.active').hide();
-
-  $('.next-btn').click(function(event) {
-      // Prevent the default action
-      event.preventDefault();
-
-      var currentSection = $(this).closest('.form-group');
-      currentSection.fadeOut(400, function() {
-          var nextSection = currentSection.next('.form-group');
-          nextSection.fadeIn(400);
-
-          counter++;
-      });
-
-      console.log(counter);
-
-      if (counter === 8){
-            $('.btn').removeClass('hide');
-            $('.btn').addClass('reveal');
-          }
+    showCurrentGroup();
   });
 
-  
+  $("#arrow-down").click(function () {
+    if (currentIndex < formGroups.length - 1) {
+      currentIndex++;
+      console.log(currentIndex);
+    }
+
+    if (currentIndex === 8) {
+      $(".btn").removeClass("hide");
+    }
+
+    showCurrentGroup();
+  });
+
+  showCurrentGroup();
+
+  // Hide all sections except the first one
+  $(".form-group").not(".active").hide();
+
+  $(".next-btn").click(function (event) {
+    // Prevent the default action
+    event.preventDefault();
+
+    var currentSection = $(this).closest(".form-group");
+    currentSection.fadeOut(400, function () {
+      var nextSection = currentSection.next(".form-group");
+      nextSection.fadeIn(400);
+    });
+
+    currentIndex++;
+    console.log(currentIndex);
+
+    if (currentIndex === 8) {
+      $(".btn").removeClass("hide");
+    }
+  });
 });
